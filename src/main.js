4b9c2fbdcb971dca494911c547c30caecddc251d
galio <codebygalio@gmail.com>
2023-09-16 12:45:16 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import 'normalize.css/normalize.css'
+import '@/assets/styles/styles.scss'
 
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'amfe-flexible'
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
+    <HomeHeader/>
  <MySwiper/>
  test
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/HomeHeader'
 import MySwiper from './components/MySwiper'
 export default {
   name: 'Home',
   components:{
+    HomeHeader,
     MySwiper
   }
 
