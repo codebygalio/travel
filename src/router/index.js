0b4d32b6a9a8e0a588ea4f9bacf7f745906a6d8b
galio <codebygalio@gmail.com>
2023-09-18 18:49:40 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -14,12 +14,20 @@ const routes = [
     name: 'City',
     component: () => import( '@/views/city/index.vue')
   },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: () => import( '@/views/detail/index.vue')
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior () {
+    return { x: 0,y: 0 }
+     }
+   
 })
 
 export default router
---
+++ src/views/detail/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="detail-container">
+        <Banner
+            :sightName="sightName"
+            :bannerImg="bannerImg"
+            :bannerImgs="gallaryImgs"
+        ></Banner>
+        <Header/>
+          <div class="content">
+      <List :list="list"></List>
+    </div>
+    </div>
+</template>
+<script>
+import axios from "axios";
+import Banner from "./components/Banner";
+import Header from './components/Header';
+import List from './components/List'
+export default {
+    name: "Detail",
+    components: {
+        Banner,
+        Header,
+        List
+    },
+    data() {
+        return {
+            sightName: "",
+            bannerImg: "",
+            gallaryImgs: [],
+            list: [],
+        };
+    },
+    created() {
+        this.initList();
+    },
+    methods: {
+        async initList() {
+            const {data: { data }} = await axios.get("/mock/detail.json");
+            this.sightName = data.sightName;
+            this.bannerImg = data.bannerImg;
+            this.gallaryImgs = data.gallaryImgs;
+            this.list = data.categoryList;
+        },
+    },
+};
+</script>
+<style lang="scss" scoped>
+  .content{
+    height: 50rem;
+  }
+    
+</style>
